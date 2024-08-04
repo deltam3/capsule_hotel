0b1848ca30da5f8e7a745db70fcfa0fc8a5feebd
deltam3 <practicect@proton.me>
2024-08-04 14:21:39 +0900

    temp fix3

--- capsule_hotel/vite.config.ts
+++ capsule_hotel/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Customize build options as needed
+    target: "esnext", // Ensure compatibility with modern JavaScript
+  },
+});
